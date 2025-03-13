b5c39118fd3a6188f6f9b97eceadb71403b3e27b
Leonardo-Corazzini <leocora10@gmail.com>
2025-03-13 12:54:15 +0100

    add bonus 1

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ async function getDashboardData(query) {
     const promise2 = fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/weathers?search=${query}`)
     const promise3 = fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${query}`)
     const result = await Promise.all([promise1, promise2, promise3])
-    console.log(result[1])
+    console.log(result)
     const data = {
-        city: result[0][0].name,
-        country: result[0][0].country,
-        temperature: result[1][0].temperature,
-        weather: result[1][0].weather_description,
-        airport: result[2][0].name
+        city: result[0][0] ? result[0][0].name : null,
+        country: result[0][0] ? result[0][0].country : null,
+        temperature: result[1][0] ? result[1][0].temperature : null,
+        weather: result[1][0] ? result[1][0].weather_description : null,
+        airport: result[2][0] ? result[2][0].name : null
     }
     return data
 
@@ -33,13 +33,24 @@ async function getDashboardData(query) {
 
 
 
-getDashboardData('london')
+
+
+
+getDashboardData('vienna')
     .then(data => {
         console.log('Dasboard data:', data);
-        console.log(
-            `${data.city} is in ${data.country}.\n` +
-            `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n` +
-            `The main airport is ${data.airport}.\n`
-        );
+        if (data.temperature) {
+            console.log(
+                `${data.city} is in ${data.country}.\n` +
+                `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n` +
+                `The main airport is ${data.airport}.\n`
+            );
+        } else {
+            console.log(
+                `${data.city} is in ${data.country}.\n` +
+                `The main airport is ${data.airport}.\n`
+            );
+        }
+
     })
     .catch(error => console.error(error));
